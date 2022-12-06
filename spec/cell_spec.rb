d617dfc102c9b1233b0224744832034e2180b69a
Elle Majors <hellolovely@ellemajors>
2022-12-06 11:29:10 -0700

    feat: #initialize it has coordinates and ship

---
+++ spec/cell_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require './lib/ship'
+require './lib/cell'
+
+describe Cell do
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      cell = Cell.new('B4')
+
+    expect(cell).to be_a(Cell)
+    expect(cell.coordinate).to eq('B4')
+    end
+
+    it 'has empty cell by default' do
+      cell = Cell.new('B4')
+
+    expect(cell.ship).to eq(nil)
+    expect(cell.empty?).to be true
+    end
+  end
