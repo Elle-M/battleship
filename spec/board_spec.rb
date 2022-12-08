d5d1ca260bd85a3dcbbbc4195febf4ec14a492c5
Caleb Thomas <cjthomas00@comcast.net>
2022-12-07 19:19:56 -0700

    test: #valid_coordinate completed

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,30 +3,23 @@ require './lib/ship'
 require './lib/cell'
 require './lib/board'
 
-describe Board do
-  let(:cells) { { cell_1: "A1",
-                      cell_2: "A2",
-                      cell_3: "A3",
-                      cell_4: "A4",
-                      cell_5: "A1",
-                      cell_6: "A2",
-                      cell_7: "A3",
-                      cell_8: "A4",
-                      cell_9: "A1",
-                      cell_10: "A2",
-                      cell_11: "A3",
-                      cell_12: "A4",
-                      cell_13: "A1",
-                      cell_14: "A2",
-                      cell_15: "A3",
-                      cell_16: "A4"
-                } }
-    let(:board) { Board.new(cells) }
+describe Board do  
+  let(:board) { Board.new }
 
   describe '#initialize' do
     it 'exists and has attributes' do   
     expect(board).to be_a(Board)
     expect(board.cells.length).to eq(16)
+    end
+  end
+
+  describe '#valid coordinate?' do
+    it 'has valid coordinates' do
+    expect(board.valid_coordinate?("A1")).to be true 
+    expect(board.valid_coordinate?("D4")).to be true
+    expect(board.valid_coordinate?("A5")).to eq(false)
+    expect(board.valid_coordinate?("E1")).to eq(false)
+    expect(board.valid_coordinate?("A22")).to eq(false)
     end
   end
 end  
