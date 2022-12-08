54fe5b2c86151390a64433720bad07c37ebcaa77
Elle Majors <hellolovely@ellemajors>
2022-12-07 19:57:16 -0700

    test: #valid_placement

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,7 +5,9 @@ require './lib/board'
 
 describe Board do  
   let(:board) { Board.new }
-
+  let(:crusier) { Ship.new("Cruiser", 3) }
+  let(:submarine) { Ship.new("Submarine", 2) }
+  
   describe '#initialize' do
     it 'exists and has attributes' do   
     expect(board).to be_a(Board)
@@ -20,6 +22,17 @@ describe Board do
     expect(board.valid_coordinate?("A5")).to eq(false)
     expect(board.valid_coordinate?("E1")).to eq(false)
     expect(board.valid_coordinate?("A22")).to eq(false)
+    end
+  end
+
+  describe '#valid_placement?' do
+    it 'has valid ship placement' do
+    
+    expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
     end
   end
 end  
