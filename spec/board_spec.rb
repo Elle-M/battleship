4fbce662f990b1a07bb05b5623992d32ded98af5
Elle Majors <hellolovely@ellemajors>
2022-12-09 15:58:00 -0700

    test: #place

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -28,7 +28,6 @@ describe Board do
 
   describe '#valid_placement?' do
     it 'has valid length' do
-      #require 'pry'; binding.pry
     expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
     expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be false
     end
@@ -40,12 +39,26 @@ describe Board do
     expect(board.valid_placement?(submarine, ["C1", "B1"])).to be false
     expect(board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to be true
     expect(board.valid_placement?(submarine, ["B1", "C1"])).to be true
+    expect(board.valid_placement?(cruiser, ["F1", "F2", "F3"])).to be false
     end
 
     it 'cannot be diagonal' do
-      #require 'pry'; binding.pry
     expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be false
     expect(board.valid_placement?(submarine, ["C2", "D3"])).to be false
+    end
+  end
+
+  describe '#place' do
+    it 'places ships' do
+      board.place(cruiser, ["A1", "A2", "A3"])
+      cell_1 = board.cells["A1"]
+      cell_2 = board.cells["A2"]
+      cell_3 = board.cells["A3"]
+      
+    expect(cell_1.ship).to eq(cruiser)
+    expect(cell_2.ship).to eq(cruiser)
+    expect(cell_3.ship).to eq(cruiser)
+    expect(cell_3.ship == cell_2.ship).to be true
     end
   end
 end  
