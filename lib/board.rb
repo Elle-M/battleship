ae29662d024b328311a19f4c530919950d4f0b2a
Caleb Thomas <cjthomas00@comcast.net>
2022-12-08 21:00:20 -0700

    add #helper for #valid_placement

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,6 @@
 class Board
-  attr_reader #:cells
+  attr_reader :cells
+              
               
 
   def initialize
@@ -7,30 +8,57 @@ class Board
   end
 
   def cells 
-    cells = {cell_1: "A1",
-    cell_2: "A2",
-    cell_3: "A3",
-    cell_4: "A4",
-    cell_5: "B1",
-    cell_6: "B2",
-    cell_7: "B3",
-    cell_8: "B4",
-    cell_9: "C1",
-    cell_10: "C2",
-    cell_11: "C3",
-    cell_12: "C4",
-    cell_13: "D1",
-    cell_14: "D2",
-    cell_15: "D3",
-    cell_16: "D4"}
+     {
+    "A1": Cell.new("A1"),
+    "A2": Cell.new("A2"),
+    "A3": Cell.new("A3"),
+    "A4": Cell.new("A4"),
+    "B1": Cell.new("B1"),
+    "B2": Cell.new("B2"),
+    "B3": Cell.new("B3"),
+    "B4": Cell.new("B4"),
+    "C1": Cell.new("C1"),
+    "C2": Cell.new("C2"),
+    "C3": Cell.new("C3"),
+    "C4": Cell.new("C4"),
+    "D1": Cell.new("D1"),
+    "D2": Cell.new("D2"),
+    "D3": Cell.new("D3"),
+    "D4": Cell.new("D4")
+  }
   end
   
   def valid_coordinate?(cell)
-     cells.value?(cell)
-    # require 'pry'; binding.pry
+      @cells.key?(cell)
   end
 
-  def valid_placement?(ship, cell)
-    cells.value?(cell)
+  def valid_placement?(ship_type, coordinates)
+      ship_type.length == coordinates.length &&
+      horizontal_valid_placement?(coordinates)
   end
+
+  def duplicate_letter?(coordinates) #Will return true for ["A1", "A2", "A3"] or false ["A1", "B1", "C1"] - both valid placements
+      letter = coordinates.map { |coordinate| coordinate.split('').first.ord}
+      letter.each_cons(2).all? { |letter_1, letter_2| letter_1 == letter_2 } 
+  end
+
+  def number_consecutive?(coordinates) #Will return true for ["A1", "A2", "A3"] or false ["A1"", "B1", "C1] - both valid placements.
+      number = coordinates.map { |coordinate| coordinate.split('').last.to_i}
+      number.each_cons(2).all? { |num_1, num_2| num_2.to_i - 1 == num_1 } 
+  end
+
+  def horizontal_valid_placement?(coordinates) #Returns true for ["A2", "A3", "A4"]
+      duplicate_letter?(coordinates) && number_consecutive?(coordinates)
+  end
+
+  def consecutive_letter(coordinates) #Will return false for ["A2", "A3", "A4"]
+      letter = coordinates.map { |coordinate| coordinate.split('').first.ord}
+      letter.each_cons(2).all? { |letter_1, letter_2| letter_2.ord - 1 == letter_1 } #true
+  end
+
+  def number_duplicate(coordinates) # - won't run correctly in pry. Says ["A2", "A3", "A4"] eq true for duplicate numbers. should be false. 
+      number = coordinates.map { |coordinate| coordinate.split('').last.to_i }
+      number.each_cons(2).all? { |num_1, num_2| num_2 - 1 == num_1 } #false
+  end
+  
 end
