2862dc5aca69a0bae82a11e0149d1bc45c547905
Caleb Thomas <cjthomas00@comcast.net>
2022-12-08 21:01:03 -0700

    add consecutive and diagonal tests.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,7 +5,7 @@ require './lib/board'
 
 describe Board do  
   let(:board) { Board.new }
-  let(:crusier) { Ship.new("Cruiser", 3) }
+  let(:cruiser) { Ship.new("Cruiser", 3) }
   let(:submarine) { Ship.new("Submarine", 2) }
   
   describe '#initialize' do
@@ -17,22 +17,32 @@ describe Board do
 
   describe '#valid coordinate?' do
     it 'has valid coordinates' do
-    expect(board.valid_coordinate?("A1")).to be true 
-    expect(board.valid_coordinate?("D4")).to be true
-    expect(board.valid_coordinate?("A5")).to eq(false)
-    expect(board.valid_coordinate?("E1")).to eq(false)
-    expect(board.valid_coordinate?("A22")).to eq(false)
+
+    expect(board.valid_coordinate?(:A1)).to be true 
+    expect(board.valid_coordinate?(:D4)).to be true
+    expect(board.valid_coordinate?(:A5)).to eq(false)
+    expect(board.valid_coordinate?(:E1)).to eq(false)
+    expect(board.valid_coordinate?(:A22)).to eq(false)
     end
   end
 
   describe '#valid_placement?' do
-    it 'has valid ship placement' do
-    
+    it 'has valid length' do
+      #require 'pry'; binding.pry
     expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
     expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+    end
+
+    it 'has consecutive coordinates' do
     expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
     expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
     expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+    end
+
+    xit 'cannot be diagonal' do
+    expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
     end
   end
 end  
