7b33da183095dd252e6d0a0e24039c4e0cbd466a
Elle Majors <hellolovely@ellemajors>
2022-12-11 09:50:07 -0700

    refactor: add let statement and spacing for better readabilty

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,11 +1,11 @@
 require 'rspec'
 require './lib/ship'
 
-RSpec.describe Ship do
+describe Ship do
+  let(:cruiser) { Ship.new("Cruiser", 3) }
+
   describe '#initialize' do
     it 'exists and has attributes' do
-      cruiser = Ship.new("Cruiser", 3)
-
     expect(cruiser.name).to eq('Cruiser')
     expect(cruiser.length).to eq(3)
     expect(cruiser.health).to eq(3)
@@ -14,30 +14,30 @@ RSpec.describe Ship do
 
   describe '#sunk?' do
     it 'has not been sunk by default' do
-      cruiser = Ship.new("Cruiser", 3)
-
     expect(cruiser.sunk?).to eq(false)
     end
   end
 
   describe '#hit' do
     it 'has been hit' do
-      cruiser = Ship.new("Cruiser", 3)
-
       cruiser.hit
+
       expect(cruiser.health).to eq(2)
     end
 
     it 'has been sunk' do
-      cruiser = Ship.new("Cruiser", 3)
-      
       cruiser.hit
+
       expect(cruiser.health).to eq(2)
       expect(cruiser.sunk?).to eq(false)
+
       cruiser.hit
+
       expect(cruiser.health).to eq(1)
       expect(cruiser.sunk?).to eq(false)
+
       cruiser.hit
+
       expect(cruiser.sunk?).to be true
     end
   end
