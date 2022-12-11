79b493f137e5998eb6643e0041fbf478dea9025e
Caleb Thomas <cjthomas00@comcast.net>
2022-12-11 15:59:42 -0700

    Begin #run_game build out.

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,20 @@
+require 'rspec'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/game'
+
 class Game 
-
+  attr_reader :comp_board
   def initialize
-
+    @comp_board = Board.new
+    @player_board = Board.new
+    @comp_cruiser = Ship.new("Cruiser", 3) 
+    @comp_sub = Ship.new("Submarine", 2)
+    @player_cruiser = Ship.new("Cruiser", 3)
+    @player_sub =  Ship.new("Submarine", 2)
+    @comp_player(#put ships here)
+    @player(#put ships here)
   end
 
   def start
@@ -16,10 +29,35 @@ class Game
       p 'Please enter p to play or enter q to quit.'
     end
   end
+
+  def run_game
+# Computer can place ships randomly in valid locations
+      @comp_board
+      #comp_player(@comp_ships)
+      #comp_player.comp_board.place.rand
+
+#       I have laid out my ships on the grid.
+# You now need to lay out your two ships.
+# The Cruiser is three units long and the Submarine is two units long.
+#   1 2 3 4
+# A . . . .
+# B . . . .
+# C . . . .
+# D . . . .
+# Enter the squares for the Cruiser (3 spaces): player.board.place
+#player(player_ships)
+
+# User can enter valid sequences to place both ships
+# Entering invalid ship placements prompts user to enter valid placements
+  end
+
+  def comp_player(ships)
+
+  end
 end
 game = Game.new
 game.start
-
+require 'pry'; binding.pry
 #   Functionality Checklist
 
 # This checklist summarizes all of the functionality you are expected to build. This will be used to assess the completion of your project:
@@ -27,9 +65,6 @@ game.start
 # Main Menu:
 
 
-# Computer can place ships randomly in valid locations
-# User can enter valid sequences to place both ships
-# Entering invalid ship placements prompts user to enter valid placements
 # Turn:
 
 # User board is displayed showing hits, misses, sunken ships, and ships
