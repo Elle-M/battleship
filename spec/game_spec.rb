c783557162bda5466d3e382ee57b3821817be8dc
Caleb Thomas <cjthomas00@comcast.net>
2022-12-12 14:14:55 -0700

    test: add #valid_comp_coord test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,11 +6,23 @@ require './lib/game'
 
 describe Game do
   let(:game) { Game.new }
-  
+
   describe '#initialize' do
     it 'exists' do 
       
     expect(game).to be_a(Game)
     end
+
+  describe '#valid_comp_coord' do
+    it 'has valid_comp_coord' do
+      comp_cruiser = Ship.new("Cruiser", 3)  
+      comp_sub = Ship.new("Submarine", 2)
+      cells = game.valid_comp_coord(comp_cruiser)
+      #require 'pry'; binding.pry
+      expect(game.valid_comp_coord(comp_cruiser).length).to eq(comp_cruiser.length)
+      expect(game.valid_comp_coord(comp_sub).length).to eq(comp_sub.length)
+      expect(game.comp_board.valid_placement?(comp_cruiser, cells)).to be true
+   end
   end
+end
 end
