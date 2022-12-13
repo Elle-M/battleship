c025b066aa9efacee00b8401bb959b20ec2de4a3
Elle Majors <hellolovely@ellemajors>
2022-12-13 16:48:23 -0700

    refactor: line allignment

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -6,15 +6,15 @@ describe Ship do
 
   describe '#initialize' do
     it 'exists and has attributes' do
-    expect(cruiser.name).to eq('Cruiser')
-    expect(cruiser.length).to eq(3)
-    expect(cruiser.health).to eq(3)
+      expect(cruiser.name).to eq('Cruiser')
+      expect(cruiser.length).to eq(3)
+      expect(cruiser.health).to eq(3)
     end
   end
 
   describe '#sunk?' do
     it 'has not been sunk by default' do
-    expect(cruiser.sunk?).to eq(false)
+      expect(cruiser.sunk?).to eq(false)
     end
   end
 
@@ -22,23 +22,23 @@ describe Ship do
     it 'has been hit' do
       cruiser.hit
 
-    expect(cruiser.health).to eq(2)
+      expect(cruiser.health).to eq(2)
     end
 
     it 'has been sunk' do
       cruiser.hit
 
-    expect(cruiser.health).to eq(2)
-    expect(cruiser.sunk?).to eq(false)
+      expect(cruiser.health).to eq(2)
+      expect(cruiser.sunk?).to eq(false)
 
       cruiser.hit
 
-    expect(cruiser.health).to eq(1)
-    expect(cruiser.sunk?).to eq(false)
+      expect(cruiser.health).to eq(1)
+      expect(cruiser.sunk?).to eq(false)
 
       cruiser.hit
 
-    expect(cruiser.sunk?).to be true
+      expect(cruiser.sunk?).to be true
     end
   end
 end
