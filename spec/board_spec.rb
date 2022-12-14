ab4719adca0660ee6873443d0862da54909d399b
Elle Majors <hellolovely@ellemajors>
2022-12-14 08:32:51 -0700

    refactor: test line allignment/spacing

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,6 +7,7 @@ describe Board do
   let(:board) { Board.new }
   let(:cruiser) { Ship.new("Cruiser", 3) }
   let(:submarine) { Ship.new("Submarine", 2) }
+
   describe '#initialize' do
     it 'exists and has attributes' do   
     expect(board).to be_a(Board)
@@ -54,10 +55,11 @@ describe Board do
       cell_1 = board.cells[:A1]
       cell_2 = board.cells[:A2]
       cell_3 = board.cells[:A3]
-    expect(cell_1.ship).to eq(cruiser)
-    expect(cell_2.ship).to eq(cruiser)
-    expect(cell_3.ship).to eq(cruiser)
-    expect(cell_3.ship == cell_2.ship).to be true
+
+      expect(cell_1.ship).to eq(cruiser)
+      expect(cell_2.ship).to eq(cruiser)
+      expect(cell_3.ship).to eq(cruiser)
+      expect(cell_3.ship == cell_2.ship).to be true
     end
   end
 
@@ -65,7 +67,7 @@ describe Board do
     it 'will not overlap' do
       board.place(cruiser, ["A1", "A2", "A3"])
       submarine = Ship.new("Submarine", 2)
-      expect(board.valid_placement?(submarine, ["A1", "B1"])).to eq(false)
+      expect(board.valid_placement?(submarine, ["A1", "B1"])).to be false
     end
   end
 
